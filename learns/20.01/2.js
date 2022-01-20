8f148d2f80b1a79ffa55de1edfec26f5ddc77eff
alex_kraken <Lihach.ok@gmail.com>
2022-01-20 21:21:58 +0300

    klass work 20.01

--- learns/20.01/2.js
+++ learns/20.01/2.js
@@ -5,3 +5,9 @@ let res = [];
 res = arr.map((t) => t[0].toUpperCase() + t.toLowerCase().slice(1));
 
 console.log(res);
+
+arr.forEach(
+  (t, i, a) => (a[i] = t[0].toUpperCase() + t.toLowerCase().slice(1))
+);
+
+console.log(arr);
