7082dc4133f166ea5807ae84225f0438514f14eb
alex <lihach.ok@gmail.com>
2022-02-08 16:40:36 +0300

    home work 08.feb_4

--- DZ/02/08/1.js
+++ DZ/02/08/1.js
@@ -15,11 +15,11 @@
 //       },
 //       ....
 //   ]
-// Полученный массив преобразуйте заменив phone на сумму ТОЛЬКО четных цифр из этого же свойства
-// Отсортируйте массив по name
-// Удалите из массив всех в номере дома которых есть цифра 1
-// Разделите индекс всех на 2 и округлите результат до целого вверх
-// Выведите результат
+// (!) Полученный массив преобразуйте заменив phone на сумму ТОЛЬКО четных цифр из этого же свойства
+// (!) Отсортируйте массив по name
+// (!) Удалите из массив всех в номере дома которых есть цифра 1
+// (!) Разделите индекс всех на 2 и округлите результат до целого вверх
+// (!) Выведите результат
 "use strict";
 const request = new XMLHttpRequest();
 
@@ -34,14 +34,26 @@ request.onload = function () {
   const f = object.map((user) => {
     return {
       id: user.id,
-      name: user.name.slice(0, user.name.indexOF(" ")).ToUpperCase(), // 'FIRSTNAME', //Возьмите только имя (часть строки до первого пробела), сделайте все буквы заглавными
-      phone: user.phone
+      name: user.name.slice(0, user.name.indexOf(" ")).toUpperCase(), // 'FIRSTNAME', //Возьмите только имя (часть строки до первого пробела), сделайте все буквы заглавными
+      phoneStr: user.phone,
+      phoneNum: user.phone
         .split("")
         .filter((e) => Number.isInteger(parseInt(e)))
         .join(""), // 'номер телефона', // Удалите все кроме цифр
+      phoneSumEvenIntem: user.phone
+        .split("")
+        .filter((e) => Number.isInteger(parseInt(e)))
+        .reduce((s, e, i) => (s += e * (i % 2)), 0),
+
       location: {
         street: user.address.street, //'Улица',
         index: user.address.zipcode, //'Почтовый индекс',
+        index2: Math.ceil(
+          user.address.zipcode
+            .split("")
+            .filter((e) => Number.isInteger(parseInt(e)))
+            .join("") / 2
+        ), //'Почтовый индекс',
         number: user.address.suite
           .split("")
           .filter((e) => Number.isInteger(parseInt(e)))
@@ -49,6 +61,19 @@ request.onload = function () {
       },
     };
   });
+  console.log(
+    f
+      .sort((e1, e2) => {
+        if (e1.name > e2.name) {
+          return 1;
+        }
+        if (e1.name < e2.name) {
+          return -1;
+        }
+        return 0;
+      })
+      .filter((e) => e.location.number.indexOf("1") === -1)
+  );
 };
 
 request.send();
