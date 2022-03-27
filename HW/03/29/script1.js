b7492425db6ce0f693a30d1fdd132d9a08cb0ac7
alex <lihach.ok@gmail.com>
2022-03-27 15:44:47 +0300

    finish homeork 29.03

--- HW/03/29/script1.js
+++ HW/03/29/script1.js
@@ -1,23 +1,43 @@
-const request = url => {
-    return new Promise((success, error) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = () => {
-            xhr.status === 200 ? success(xhr.response) : error(xhr.statusText);
-        };
-        xhr.onerror = () => error(xhr.statusText);
-        xhr.send();
+const request = (url, success, error) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+        xhr.status === 200 ? success(xhr.response) : error(xhr.statusText);
+    };
+    xhr.onerror = () => error(xhr.statusText);
+    xhr.send();
+};
+
+document.querySelector('#get-text').onclick = () => {
+    request('https://itchief.ru/examples/ajax/01.html', data => {
+        document.querySelector('#result').innerHTML = data;
+    }, error => {
+        console.error(error);
     });
 };
 
+// const request = url => {
+//     return new Promise((success, error) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('GET', url);
+//         xhr.onload = () => {
+//             xhr.status === 200 ? success(xhr.response) : error(xhr.statusText);
+//         };
+//         xhr.onerror = () => error(xhr.statusText);
+//         xhr.send();
+//     });
+// };
+//
+//
+// document.querySelector('#get-text').onclick = () => {
+//     request('https://itchief.ru/examples/ajax/01.html')
+//         .then(data => {
+//             document.querySelector('#result').innerHTML = data;
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         });
+//
+// };
 
-document.querySelector('#get-text').onclick = () => {
-    request('https://jsonplaceholder.typicode.com/todos')
-        .then(data => {
-            document.querySelector('#result').innerHTML = data;
-        })
-        .catch(error => {
-            console.error(error);
-        });
 
-};
--- HW/03/29/script3.js
+++ HW/03/29/script3.js
@@ -1,30 +1,49 @@
-setTimeout(() => {
-    document.body.querySelector('.modal').modal('show');
-}, 3000);
 
+const loadJs = (src) => {
+    return new Promise((success, error)=>{
+        const script = document.createElement('script');
+        script.onload = success();
+        script.onerror = error();
+        script.src = src;
+        document.head.append(script);
 
-//
+    });
+};
+
+loadJs('https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js')
+    .then(()=>{
+        console.log(' первый файл загружен');
+        return loadJs('https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js');
+    })
+    .then (()=>{
+        console.log(' второй файл загружен');
+        setTimeout(() => {
+            console.log(' таймаут сработал');
+            $('.modal').modal('show');
+        }, 3000);
+    });
+
 // const loadJs = (src, success, error) => {
 //     const script = document.createElement('script');
 //     script.onload = success;
 //     script.onerror = error;
 //     script.src = src;
-//     document.body.append(script);
+//     document.head.append(script);
 // };
+
+/**
+ * код для открытия модального окна нужно выполнить после загрузке bootstrap.bundle.min.js, а bootstrap.bundle.min.js следует загрузить только после загрузки jquery.min.js
+ */
+
 //
 //
-//
-// /**
-//  * код для открытия модального окна нужно выполнить после загрузке bootstrap.bundle.min.js, а bootstrap.bundle.min.js следует загрузить только после загрузки jquery.min.js
-//  */
-//
-// loadJs('https://code.jquery.com/jquery-3.2.1.slim.min.js',
+// loadJs('https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
 //     () => {
-//         loadJs('https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
+//         loadJs('https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js',
 //             () => {
 //                 setTimeout(() => {
-//                     document.body.querySelector('.modal').modal('show');
-//                 }, 3000);
+//                     $('.modal').modal('show');
+//                 }, 1000);
 //             },
 //             () => {
 //                 console.error('Ошибка при загрузки скриптов!');
@@ -35,5 +54,3 @@ setTimeout(() => {
 //         console.error('Ошибка при загрузки скриптов!');
 //     }
 // );
-
-
